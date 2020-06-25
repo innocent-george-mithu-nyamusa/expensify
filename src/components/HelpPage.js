c0dceab477e3a1d09448a362f056dc8977b296e7
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-06-26 01:38:37 +0200

    fixed wrong and unused react-router-dom import

--- src/components/HelpPage.js
+++ src/components/HelpPage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-mod';
 
 const expensifyHelpPage = () => (
   <div>
     <h1>Welcome to Expensify Help </h1>
-    <h2>Help</h2>
   </div>
 );
 
