ecedc744e0111252f5ee5b52aa44a6d92401a6ca
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-06-26 01:37:24 +0200

    fixed ExpenseForm import Error

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ExpenseForm from 'expenseForm';
+import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
