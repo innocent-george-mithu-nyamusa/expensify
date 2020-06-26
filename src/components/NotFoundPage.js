499bbeec78d4aecc5918dc458ed3789aa705d4f5
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-06-26 16:24:42 +0200

    removed  <link> line

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const notFoundPage = () => (
   <div>
     <p>Page Not found 404</p>
-    <Link to="/">Go home</Link>
   </div>
 );
 
