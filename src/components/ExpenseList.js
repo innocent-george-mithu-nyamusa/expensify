880c64d3f122d6eb4d04431f0e6391cd39d34ba2
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-06-26 16:11:16 +0200

    fixed connect

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import Expense from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 export const ExpenseList = (props) => (
