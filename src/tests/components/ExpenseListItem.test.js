0775df1a121efb4909f92b3c9d36c8e33299912f
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-06-28 21:04:24 +0200

    fixed shallow import

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import shallow from 'enzyme';
+import { shallow } from 'enzyme';
 import ExpenseListItem from './../../components/ExpenseListItem';
 import expenses from './../fixtures/expenses';
 
