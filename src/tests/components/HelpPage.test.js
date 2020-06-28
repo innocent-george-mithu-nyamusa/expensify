4565f8a8da9226ec51d0e3c197e4d82dd4a21434
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-06-28 21:05:17 +0200

    fixed shallow import

--- src/tests/components/HelpPage.test.js
+++ src/tests/components/HelpPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import shallow from 'enzyme';
+import { shallow } from 'enzyme';
 import HelpPage from './../../components/HelpPage';
 
 test('Should render Not Found page', () => {
