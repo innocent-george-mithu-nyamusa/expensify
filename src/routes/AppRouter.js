bc6e1c63b40eca50f4dae7c4ec96fa7f2cd8174b
innocent-george-mithu-nyamusa <innocentnyamusa@gmail.com>
2020-06-30 23:59:54 +0200

    updated some imports

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Header from './../components/Header';
-import expensifyDashboardPage from './../components/dashboardPage';
-import expensifyEditExpensePage from './../components/editExpensePage';
-import expensifyCreateExpensePage from './../components/addExpensePage';
-import expensifyHelpPage from './../components/helpPage';
-import notFoundPage from './../components/notFoundPage';
+import Header from '../components/Header';
+import DashboardPage from '../components/DashboardPage';
+import EditExpensePage from '../components/EditExpensePage';
+import AddExpensePage from '../components/AddExpensePage';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Header />
       <Switch>
-        <Route path="/" component={expensifyDashboardPage} exact={true} />
-        <Route path="/create" component={expensifyCreateExpensePage} />
-        <Route path="/edit/:id" component={expensifyEditExpensePage} />
-        <Route path="/help" component={expensifyHelpPage} />
-        <Route component={notFoundPage} />
+        <Route path="/" component={DashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit/:id" component={EditExpensePage} />
+        <Route path="/help" component={HelpPage} />
+        <Route component={NotFoundPage} />
       </Switch>
     </div>
   </BrowserRouter>
